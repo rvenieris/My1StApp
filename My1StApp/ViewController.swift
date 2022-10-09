8d892125524cbae219337b3595d522df2cb5f4ec
Ricardo Venieris <rvenieris@gmail.com>
2022-10-09 18:37:26 -0300

    Initial Commit

---
+++ My1StApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My1StApp
+//
+//  Created by Ricardo Venieris on 09/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
